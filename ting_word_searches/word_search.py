3c01507016500bcea29ee99e03887b12d2729f9f
lbseven7 <leobarbosapinturas@gmail.com>
2023-02-09 21:11:37 -0300

    feat: Implementa uma função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,17 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for file_data in instance.queue:
+        lines = [index + 1 for index,
+                 line in enumerate(file_data['linhas_do_arquivo'])
+                 if word.lower() in line.lower()]
+        if lines:
+            result.append({
+                'palavra': word,
+                'arquivo': file_data['nome_do_arquivo'],
+                'ocorrencias': [{'linha': line_number}
+                                for line_number in lines]
+            })
+    return result
 
 
 def search_by_word(word, instance):
